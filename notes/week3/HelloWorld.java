c0fe5cd60ba062b57bfb365cb47cd7deab646485
SunXun <3544360034@qq.com>
2026-06-23 12:49:40 +0800

    完整第三周作业

--- notes/week3/HelloWorld.java
+++ notes/week3/HelloWorld.java
@@ -1,22 +1,19 @@
 public class HelloWorld {
-    public static void printTip(String text){
-        System.out.println("自定义方法："+text);
-    }
     public static void main(String[] args) {
-        int count=5;
-        double score=95.0;
-        String course="Java第三周学习";
-        if(score>90){
-            System.out.println(course+"成绩优秀");
+        int loopCount = 4;
+        double score = 88.5;
+        boolean isComplete = true;
+        if (isComplete && score >= 80) {
+            System.out.println("作业完成，当前得分：" + score);
+        } else {
+            System.out.println("作业未达标，需要修改完善");
         }
-        else{
-            System.out.println(course+"成绩良好");
+        System.out.println("\n循环输出：");
+        for (int i = 1; i <= loopCount; i++) {
+            printLine(i);
         }
-        System.out.println("循环打印数字");
-        for (i=0;<=count;i++){
-            System.out.print(i+" ");
-        }
-        System.out.println();
-        printTip("本周作业全部写完");
+    }
+    private static void printLine(int num) {
+        System.out.println("第" + num + "次循环打印");
     }
 }
